598d1fdb6bcfbfe90729cff53182067fa0dcc7db
zeine <michel.szinavel@gmail.com>
2021-11-16 15:55:45 -0300

    Adding better nested attributes for relation on api response

--- api/mini-sites/services/mini-sites.js
+++ api/mini-sites/services/mini-sites.js
@@ -5,4 +5,20 @@
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Promise to fetch all records
+   *
+   * @return {Promise}
+   */
+  find(params, _populate) {
+    //  return strapi.query('mini-sites').find(params, ['banners', 'banners.slides']);
+    return strapi.query('mini-sites').find(params, [
+      'navbars','navbars.content_tag', 'navbars.navbar_items', 'navbars.navbar_items.content_tag',
+      'banners', 'banners.content_tag', 'banners.slides', 'banners.slides.content_tag',
+      'carousels', 'carousels.content_tag', 'carousels.carousel_items', 'carousels.carousel_items.content_tag',
+      'filters_sections', 'filters_sections.content_tag', 'filters_sections.filters_items', 'filters_sections.filters_items.content_tag',
+      'footers', 'footers.content_tag',
+    ]);
+  },
+};
