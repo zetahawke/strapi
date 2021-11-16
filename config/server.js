205618a3310ea180acd8297f5d406bb4e6379a26
zeine <michel.szinavel@gmail.com>
2021-11-16 12:58:30 -0300

    Adding new configs to embbeded models into mini-site response

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 3001),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '403c56061cc31385787a4a689b7cce3b'),
