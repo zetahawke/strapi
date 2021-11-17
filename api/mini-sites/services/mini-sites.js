7369d2d8d9e02635bfcc427c869607d533419cdf
zeine <michel.szinavel@gmail.com>
2021-11-17 18:58:10 -0300

    Filter items with categories and products included

--- api/mini-sites/services/mini-sites.js
+++ api/mini-sites/services/mini-sites.js
@@ -17,7 +17,7 @@ module.exports = {
       'navbars','navbars.content_tag', 'navbars.navbar_items', 'navbars.navbar_items.content_tag',
       'banners', 'banners.content_tag', 'banners.slides', 'banners.slides.content_tag',
       'carousels', 'carousels.content_tag', 'carousels.carousel_items', 'carousels.carousel_items.content_tag',
-      'filters_sections', 'filters_sections.content_tag', 'filters_sections.filters_items', 'filters_sections.filters_items.content_tag',
+      'filters_sections', 'filters_sections.content_tag', 'filters_sections.filter_items', 'filters_sections.filter_items.content_tag', 'filters_sections.filter_items.filter_item_products', 'filters_sections.filter_items.filter_item_products.content_tag',
       'footers', 'footers.content_tag',
     ]);
   },
@@ -32,7 +32,7 @@ module.exports = {
       'navbars','navbars.content_tag', 'navbars.navbar_items', 'navbars.navbar_items.content_tag',
       'banners', 'banners.content_tag', 'banners.slides', 'banners.slides.content_tag',
       'carousels', 'carousels.content_tag', 'carousels.carousel_items', 'carousels.carousel_items.content_tag',
-      'filters_sections', 'filters_sections.content_tag', 'filters_sections.filters_items', 'filters_sections.filters_items.content_tag',
+      'filters_sections', 'filters_sections.content_tag', 'filters_sections.filter_items', 'filters_sections.filter_items.content_tag', 'filters_sections.filter_items.filter_item_products', 'filters_sections.filter_items.filter_item_products.content_tag',
       'footers', 'footers.content_tag',
     ]);
   },
